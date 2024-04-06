e6e85f057abb29346cc11d551a743c50c493774c
namthai03 <160681163+namthai03@users.noreply.github.com>
2024-04-06 21:31:47 +0700

    Update main.java

--- main.java
+++ main.java
@@ -8,57 +8,57 @@ class Shape {
 }
 
 class Rectangle extends Shape {
-    private double rong;
-    private double dai;
-    public Rectangle(double rong, double dai) {
-        this.rong = rong;
-        this.dai = dai;
+    private double width;
+    private double length;
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
     }
 
     @Override
     public double Dientich() {
-        return this.rong * this.dai;
+        return this.width * this.length;
     }
     @Override
     public double Chuvi() {
-        return 2 * (this.rong + this.dai);
+        return 2 * (this.width + this.length);
     }
 }
 
 class Circle extends Shape {
-    private double bankinh;
-    public Circle(double bankinh) {
-        this.bankinh = bankinh;
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
     }
 
     @Override
     public double Dientich() {
-        return 3.14 * this.bankinh * this.bankinh;
+        return 3.14 * this.radius * this.radius;
     }
     @Override
     public double Chuvi() {
-        return 2 * 3.14 * this.bankinh;
+        return 2 * 3.14 * this.radius;
     }
 }
 
 class Triangle extends Shape {
-    private double canh1;
-    private double canh2;
-    private double canh3;
-    public Triangle(double canh1, double canh2, double canh3) {
-        this.canh1 = canh1;
-        this.canh2 = canh2;
-        this.canh3 = canh3;
+    private double side1;
+    private double side2;
+    private double side3;
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
 
     @Override
     public double Dientich() {
-        double p = (this.canh1 + this.canh2 + this.canh3) / 2;
-        return Math.sqrt(p * (p - this.canh1) * (p - this.canh2) * (p - this.canh3));
+        double p = (this.side1 + this.side2 + this.side3) / 2;
+        return Math.sqrt(p * (p - this.side1) * (p - this.side2) * (p - this.side3));
     }
     @Override
     public double Chuvi() {
-        return this.canh1 + this.canh2 + this.canh3;
+        return this.side1 + this.side2 + this.side3;
     }
 }
 
