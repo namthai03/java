457d07956b28ac005272486339d8a5f3f99e5375
namthai03 <160681163+namthai03@users.noreply.github.com>
2024-04-06 21:10:20 +0700

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,6 @@ class Shape {
     public double Dientich() {
         return 0.0;
     }
-
     public double Chuvi() {
         return 0.0;
     }
@@ -11,7 +10,6 @@ class Shape {
 class Rectangle extends Shape {
     private double rong;
     private double dai;
-
     public Rectangle(double rong, double dai) {
         this.rong = rong;
         this.dai = dai;
@@ -21,7 +19,6 @@ class Rectangle extends Shape {
     public double Dientich() {
         return this.rong * this.dai;
     }
-
     @Override
     public double Chuvi() {
         return 2 * (this.rong + this.dai);
@@ -30,7 +27,6 @@ class Rectangle extends Shape {
 
 class Circle extends Shape {
     private double bankinh;
-
     public Circle(double bankinh) {
         this.bankinh = bankinh;
     }
@@ -39,7 +35,6 @@ class Circle extends Shape {
     public double Dientich() {
         return 3.14 * this.bankinh * this.bankinh;
     }
-
     @Override
     public double Chuvi() {
         return 2 * 3.14 * this.bankinh;
@@ -50,7 +45,6 @@ class Triangle extends Shape {
     private double canh1;
     private double canh2;
     private double canh3;
-
     public Triangle(double canh1, double canh2, double canh3) {
         this.canh1 = canh1;
         this.canh2 = canh2;
@@ -62,20 +56,17 @@ class Triangle extends Shape {
         double p = (this.canh1 + this.canh2 + this.canh3) / 2;
         return Math.sqrt(p * (p - this.canh1) * (p - this.canh2) * (p - this.canh3));
     }
-
     @Override
     public double Chuvi() {
         return this.canh1 + this.canh2 + this.canh3;
     }
 }
 
-
 public class main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(5, 5);
         Circle circle = new Circle(3);
         Triangle triangle = new Triangle(2, 5, 4);
-
         System.out.println("Hinh chu nhat - Dien tich: " + rectangle.Dientich() + ", Chu vi: " + rectangle.Chuvi());
         System.out.println("Hinh tron - Dien tich: " + circle.Dientich() + ", Chu vi: " + circle.Chuvi());
         System.out.println("Hinh tam giac - Dien tich: " + triangle.Dientich() + ", Chu vi: " + triangle.Chuvi());
